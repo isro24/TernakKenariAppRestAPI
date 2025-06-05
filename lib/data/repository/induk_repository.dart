f9af53cc835324c6557c20b6b6b6d3ada13b53fe
isro24 <alamat_email_anda@example.com>
2025-06-05 20:35:56 +0700

    Menambah IndukRepository pada repository

---
+++ lib/data/repository/induk_repository.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:canary_template/data/models/request/admin/induk_request_model.dart';
+import 'package:canary_template/data/models/response/get_all_induk_response_model.dart';
+import 'package:canary_template/service/service_http_client.dart';
+import 'package:dartz/dartz.dart';
+
+class IndukRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  IndukRepository(this._serviceHttpClient);
+
+  Future<Either<String, GetIndukById>> addInduk(
+    IndukRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        "admin/induk",
+        requestModel.toMap(),
+      );
+
+      if (response.statusCode == 201) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = GetIndukById.fromJson(jsonResponse);
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while adding induk: $e");
+    }
+  }
+
+  Future<Either<String, GetAllIndukModel>> getAllInduk() async {
+    try {
+      final response = await _serviceHttpClient.get("admin/induk");
+
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = GetAllIndukModel.fromJson(jsonResponse);
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while getting all induk: $e");
+    }
+  }
+}
