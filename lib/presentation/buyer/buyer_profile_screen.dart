42a45fcdba32f64c3e59ca298200b3f9efa7a4be
isro24 <alamat_email_anda@example.com>
2025-06-06 10:55:19 +0700

    Menambah BuyerProfileScreen pada buyer profile

---
+++ lib/presentation/buyer/buyer_profile_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BuyerProfileScreen extends StatefulWidget {
+  const BuyerProfileScreen({super.key});
+
+  @override
+  State<BuyerProfileScreen> createState() => _BuyerProfileScreenState();
+}
+
+class _BuyerProfileScreenState extends State<BuyerProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
