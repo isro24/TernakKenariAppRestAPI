5074df4333e44bc99429b591e6240c40cba267ea
isro24 <alamat_email_anda@example.com>
2025-06-06 10:44:37 +0700

    Menambah ProfileViewBuyer pada buyer widget

---
+++ lib/presentation/buyer/profile/widget/profile_view_buyer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProfileViewBuyer extends StatelessWidget {
+  const ProfileViewBuyer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
