ea7e2cade67c6d14a0a703ec2ec3ecee9063b944
isro24 <alamat_email_anda@example.com>
2025-06-06 10:54:08 +0700

    Menambah Text dan TextButton pada ProfileViewBuyer

--- lib/presentation/buyer/profile/widget/profile_view_buyer.dart
+++ lib/presentation/buyer/profile/widget/profile_view_buyer.dart
@@ -1,10 +1,52 @@
+import 'package:canary_template/core/core.dart';
+import 'package:canary_template/data/models/response/buyer/buyer_profile_response_model.dart';
 import 'package:flutter/material.dart';
 
 class ProfileViewBuyer extends StatelessWidget {
-  const ProfileViewBuyer({super.key});
+  final Data profile;
+  const ProfileViewBuyer({super.key, required this.profile});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Nama: ${profile.name}",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.05,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Alamat: ${profile.address}",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.04,
+              ),
+            ),
+            Text(
+              "No HP: ${profile.phone}",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.04,
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () {
+                context.pushAndRemoveUntil(
+                  const BuyerHomeScreen(),
+                  (route) => false,
+                );
+              },
+              child: const Text("Go to Home"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
