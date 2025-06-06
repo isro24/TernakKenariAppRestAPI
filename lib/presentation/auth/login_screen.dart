4fef7cea12f01761bcb7383f9eefe210e3267458
isro24 <alamat_email_anda@example.com>
2025-06-06 10:26:55 +0700

    Menambah TextField dan Button pada LoginScreen

--- lib/presentation/auth/login_screen.dart
+++ lib/presentation/auth/login_screen.dart
@@ -1,4 +1,14 @@
+import 'package:canary_template/core/components/components.dart';
+import 'package:canary_template/core/components/spaces.dart';
+import 'package:canary_template/core/core.dart';
+import 'package:canary_template/data/models/request/auth/login_request_model.dart';
+import 'package:canary_template/presentation/auth/bloc/login/login_bloc.dart';
+import 'package:canary_template/presentation/auth/register_screen.dart';
+import 'package:canary_template/presentation/buyer/buyer_profile_screen.dart';
+import 'package:canary_template/presentation/buyer/profile/widget/profile_buyer_form.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -31,6 +41,128 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Form(
+          key: _key,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SpaceHeight(170),
+                Text(
+                  'SELAMAT DATANG KEMBALI',
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SpaceHeight(30),
+                CustomTextField(
+                  controller: emailController, 
+                  label: 'Email', 
+                  validator: 'Email tidak boleh kosong',
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.email),
+                  ),
+                ),
+                const SpaceHeight(25),
+                CustomTextField(
+                  controller: passwordController, 
+                  label: 'Password', 
+                  validator: 'Password tidak boleh kosong',
+                  obscureText: !isShowPassword,
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.lock),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(isShowPassword ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        isShowPassword = !isShowPassword;
+                      });
+                    },
+                  ),
+                ),
+                const SpaceHeight(30),
+                BlocConsumer<LoginBloc, LoginState>(
+                  listener: (context, state){
+                    if (state is LoginFailure){
+                      ScaffoldMessenger.of(
+                        context,
+                        ).showSnackBar(SnackBar(content: Text(state.error)));
+                    } else if (state is LoginSuccess) {
+                      final role = state.responseModel.user?.role
+                          ?.toLowerCase();
+                      if (role == 'admin') {
+                        // context.pushAndRemoveUntil(
+                        //   const AdminConfirmScreen(),
+                        //   (route) => false,
+                        // );
+                      } else if (role == 'buyer') {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text(state.responseModel.message!)),
+                        );
+                        context.pushAndRemoveUntil(
+                          const BuyerProfileScreen(),
+                          (route) => false,
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Role tidak dikenali')),
+                        );
+                      }
+                    }
+                  },
+                  builder: (context, state) {
+                    return Button.filled(
+                      onPressed: state is LoginLoading
+                          ? null
+                          : () {
+                              if (_key.currentState!.validate()) {
+                                final request = LoginRequestModel(
+                                  email: emailController.text,
+                                  password: passwordController.text,
+                                );
+                              }
+                            },
+                      label: state is LoginLoading ? 'Memuat...' : 'Masuk',
+                    );
+                  },
+                ),
+
+                const SpaceHeight(20),
+                Text.rich(
+                  TextSpan(
+                    text: 'Belum punya akun? ',
+                    style: TextStyle(
+                      color: AppColors.grey,
+                      fontSize: MediaQuery.of(context).size.width * 0.03,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Daftar disini!',
+                        style: TextStyle(
+                          color: AppColors.primary,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            context.push(
+                              const RegisterScreen(),
+                            );
+                          },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ),
+      ),
+    );
   }
 }
